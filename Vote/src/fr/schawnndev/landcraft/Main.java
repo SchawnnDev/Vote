ffb049b0e4fb05de41d4ec82c71fe5fef71791a4
Schawnn <iperskill@gmail.com>
2014-10-10 18:39:14 +0200

    Ajout du système MySQL et ajout du fichier config.yml

--- Vote/src/fr/schawnndev/landcraft/Main.java
+++ Vote/src/fr/schawnndev/landcraft/Main.java
@@ -1,15 +1,29 @@
 package fr.schawnndev.landcraft;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+	
+	private String host = getConfig().getString("Host");
+	private String database = getConfig().getString("Database");
+	private String user = getConfig().getString("Identifiant");
+	private String password = getConfig().getString("MotDePasse");
 
 	public void onEnable() {
-
+		//TODO:	PluginManager pm = getServer().getPluginManager();
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		if(host.length() != 0 && database.length() != 0 && user.length() != 0 && password.length() != 0){
+		new SQL(host, 3306, database, user, password);
+		} else {
+			Bukkit.getLogger().severe("[Vote LandCraft] MySQL => Idenfiants faux sont egals a 0 ! (Config.yml)");
+		}
 	}
 	
 	public void onDisable() {
-
+		if(SQL.hasConnection())
+			SQL.disconnectMySQL();
 	}
 
 }
--- Vote/src/fr/schawnndev/landcraft/SQL.java
+++ Vote/src/fr/schawnndev/landcraft/SQL.java
@@ -1,5 +1,52 @@
 package fr.schawnndev.landcraft;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.bukkit.Bukkit;
+
 public class SQL {
+	
+	String ipa = null, databasea = null, usera = null, passworda = null, p = ":"; 
+	int porta;
+	
+	public SQL (String host, int port, String database, String user, String password){
+		this.ipa = host;
+		this.porta = port;
+		this.databasea = database;
+		this.usera = user;
+		this.passworda = password;
+	}
+	
+	public static Connection connection;
+	
+	public Connection connect(){
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://" + this.ipa + ":" + this.porta + "/" + this.databasea, this.usera, this.passworda);
+			Bukkit.getLogger().info("[Vote LandCraft] MySQL => En fonctionnement !");
+		} catch (SQLException e) {
+		Bukkit.getLogger().severe("[Vote LandCraft] Erreur MySQL => " + e.getMessage());
+		}
+		
+		return connection;
+	}
+	
+	public static void disconnectMySQL(){
+		try {
+			if(!connection.isClosed() || hasConnection())
+				connection.close();
+		} catch (SQLException e) {
+			Bukkit.getLogger().severe("[Vote LandCraft] Erreur MySQL => " + e.getMessage());
+		}
+	}
+	
+	public static boolean hasConnection() {
+		if(connection != null)
+			return true;
+		
+		return false;
+		
+	}
 
 }
